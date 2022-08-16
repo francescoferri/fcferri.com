504f1f5958296f0ed748e84fe0446cbbcfe3f5ad
Francesco Ferri <fcferri@outook.com>
2022-08-16 12:45:28 +0200

    fix: navbar overflow in .nav-container during button activation

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,26 +1,28 @@
 const mobileButtonOpen = document.getElementById("mobile-menu-open");
 const mobileButtonClose = document.getElementById("mobile-menu-close");
-nav = document.querySelector("nav");
+var lastScroll,
+    currentScrollTop = 0,
+    navContainer = document.querySelector(".nav-container"),
+    nav = document.querySelector("nav");
 
 mobileButtonOpen.addEventListener("click", () => {
     nav.classList.add("mobile-menu-active");
+    navContainer.style.overflow = "visible";
 });
 
 mobileButtonClose.addEventListener("click", () => {
     nav.classList.remove("mobile-menu-active");
+    navContainer.style.overflow = "hidden";
 });
 
 document.querySelectorAll(".nav-menu-item").forEach((item) => {
     item.addEventListener("click", (event) => {
         nav.classList.remove("mobile-menu-active");
+        navContainer.style.overflow = "hidden";
     });
 });
 
 // show nav on scroll up
-var lastScroll,
-    currentScrollTop = 0,
-    navContainer = document.querySelector(".nav-container");
-
 window.onscroll = function () {
     var navHeigth = navContainer.clientHeight;
     currentScrollTop = document.documentElement.scrollTop;
